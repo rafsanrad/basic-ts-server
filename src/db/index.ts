b4fdfeabf9e976882111a67377dcc1e1b9234d6a
Rafsan Rad <rafsanrad@Rafsans-MacBook-Air.local>
2026-07-04 16:49:45 +0600

    all done

--- src/db/index.ts
+++ src/db/index.ts
@@ -8,29 +8,36 @@ export const pool = new Pool({
 export const initDB = async () => {
   try {
     await pool.query(`
-           CREATE TABLE IF NOT EXISTS users(
-           id SERIAL PRIMARY KEY,
-           name VARCHAR(100) NOT NULL,
-           email VARCHAR(255) UNIQUE NOT NULL,
-           password TEXT NOT NULL,
-           role VARCHAR(20) DEFAULT 'contributor' CHECK(role IN ('contributor','maintainer')),
-           created_at TIMESTAMPTZ DEFAULT NOW(),
-           updated_at TIMESTAMPTZ DEFAULT NOW()
-           ) 
-            `);
+        CREATE TABLE IF NOT EXISTS users(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(20),
+        email VARCHAR(20) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        is_active BOOLEAN DEFAULT true,
+        age INT,
+        role VARCHAR(10) DEFAULT 'user',
+
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `);
+
     await pool.query(`
-      CREATE TABLE IF NOT EXISTS issues(
+      CREATE TABLE IF NOT EXISTS profiles(
       id SERIAL PRIMARY KEY,
-      title VARCHAR(150) NOT NULL,
-      description TEXT NOT NULL CHECK(LENGTH(description) >= 20),
-      type VARCHAR(20) NOT NULL CHECK(type IN ('bug','feature_request')),
-      status VARCHAR(20) DEFAULT 'open' CHECK(status IN ('open','in_progress','resolved')),
-      reporter_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-      created_at TIMESTAMPTZ DEFAULT NOW(),
-      updated_at TIMESTAMPTZ DEFAULT NOW()
-      )
-      `);
-    console.log("Database connected successfully!!");
+      user_id INT UNIQUE REFERENCES users(id) ON DELETE CASCADE,
+
+      bio TEXT,
+      address TEXT,
+      phone VARCHAR(15),
+      gender VARCHAR(10),
+
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+      )  
+        `);
+
+    console.log("Database connected successfully!");
   } catch (error) {
     console.log(error);
   }
